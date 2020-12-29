0464ebac8d3541f12f48de23ce0eef758ee5743e
ColasGael <gael.colas@plus.ai>
2020-12-28 22:37:32 -0800

    Add solution for days 23-24

---
+++ solver/day23.py
@@ -0,0 +1,89 @@
+def main(input_lines, part1_n_games=100, part2_n_games=10000000, part2_n_cups=1000000):
+    part1_cups = parse_input(input_lines)
+    part1_answer = "".join([str(cup) for cup in play(part1_cups, part1_n_games)[1:]])
+
+    part2_cups = part1_cups + list(range(len(part1_cups)+1, part2_n_cups+1))
+    part2_final_cup_order = play(part2_cups, part2_n_games)
+    part2_answer = part2_final_cup_order[1] * part2_final_cup_order[2]
+
+    return part1_answer, part2_answer
+
+
+def parse_input(input_lines):
+    cups = [int(char) for char in input_lines[0].strip()]
+    return cups
+
+
+def play(cups, n_games):
+    current_cup, next_cups = start(cups)
+    for i in range(n_games):
+        current_cup, next_cups = move_cups(current_cup, next_cups)
+    cups = find_final_cup_order(next_cups)
+    return cups
+
+
+def start(cups):
+    cups = [cup - 1 for cup in cups]
+    next_cups = [None] * len(cups)
+    for idx, cup in enumerate(cups[:-1]):
+        next_cups[cup] = cups[idx + 1]
+    next_cups[cups[-1]] = cups[0]
+    return cups[0], next_cups
+
+
+def move_cups(current_cup, next_cups, n_cups_removed=3):
+    removed_cups = []
+    removed_cup = next_cups[current_cup]
+    for i in range(n_cups_removed):
+        removed_cups.append(removed_cup)
+        removed_cup = next_cups[removed_cup]
+    next_cups[current_cup] = removed_cup
+
+    destination_cup = (current_cup - 1) % len(next_cups)
+    while destination_cup in removed_cups:
+        destination_cup = (destination_cup - 1) % len(next_cups)
+    next_cups[removed_cups[-1]] = next_cups[destination_cup]
+    next_cups[destination_cup] = removed_cups[0]
+
+    return removed_cup, next_cups
+
+
+def find_final_cup_order(next_cups):
+    cups = [1]
+    current_cup = next_cups[0]
+    while current_cup != 0:
+        cups.append(current_cup + 1)
+        current_cup = next_cups[current_cup]
+    return cups
+
+
+def play_naive(cups, n_games):
+    for i in range(n_games):
+        cups = move_cups(cups)
+    cups = find_final_cup_order(cups)
+    return cups
+
+
+def move_cups_naive(cups, n_cups_removed=3):
+    current_cup = cups[0]
+    removed_cups = cups[1:1 + n_cups_removed]
+    remaining_cups = cups[1 + n_cups_removed:]
+
+    destination_cup_idx, max_cup_idx = None, 0
+    for idx, cup in enumerate(remaining_cups):
+        if (current_cup > cup):
+            if (destination_cup_idx is None) or (cup > remaining_cups[destination_cup_idx]):
+                destination_cup_idx = idx
+        elif (cup > remaining_cups[max_cup_idx]):
+            max_cup_idx = idx
+    if destination_cup_idx is None:
+        destination_cup_idx = max_cup_idx
+
+    new_cups = remaining_cups[:destination_cup_idx + 1] + removed_cups + remaining_cups[destination_cup_idx+1:] + [current_cup]
+    return new_cups
+
+
+def find_final_cup_order_naive(cups):
+    first_idx = cups.index(1)
+    cups = cups[first_idx:] + cups[:first_idx]
+    return cups
---
+++ solver/day24.py
@@ -0,0 +1,95 @@
+import re
+
+import numpy as np
+
+from day11 import convolution2D
+
+
+def main(input_lines, n_days=100):
+    SUPPORTED_DIRECTIONS = ['e', 'se', 'sw', 'w', 'nw', 'ne']
+
+    tiles_directions = parse_input(input_lines, SUPPORTED_DIRECTIONS)
+    initial_black_tiles = find_black_tiles(tiles_directions)
+    part1_answer = len(initial_black_tiles)
+
+    part2_answer = np.sum(finish_exhibit(initial_black_tiles, n_days, SUPPORTED_DIRECTIONS))
+
+    return part1_answer, part2_answer
+
+
+def parse_input(input_lines, supported_directions):
+    directions_pattern = re.compile('|'.join(supported_directions))
+    tiles_directions = []
+    for input_line in input_lines:
+        tiles_directions.append(directions_pattern.findall(input_line))
+    return tiles_directions
+
+
+def find_black_tiles(tiles_directions):
+    black_tiles = []
+    for tile_directions in tiles_directions:
+        tile_coordinates = get_tile_coordinates(tile_directions)
+        if tile_coordinates in black_tiles:
+            black_tiles.remove(tile_coordinates)
+        else:
+            black_tiles.append(tile_coordinates)
+    return black_tiles
+
+
+def get_tile_coordinates(tile_directions):
+    tile_coordinates = np.array((0, 0))
+    for direction in tile_directions:
+        coordinates_update = direction_to_coordinates(direction)
+        tile_coordinates = tile_coordinates + coordinates_update
+    return tile_coordinates.tolist()
+
+
+def direction_to_coordinates(direction):
+    if direction == 'e':
+        coordinates = (0, 1)
+    elif direction == 'se':
+        coordinates = (-1, 1)
+    elif direction == 'sw':
+        coordinates = (-1, 0)
+    elif direction == 'w':
+        coordinates = (0, -1)
+    elif direction == 'nw':
+        coordinates = (1, -1)
+    elif direction == 'ne':
+        coordinates = (1, 0)
+    else:
+        raise RuntimeError("Unsupported direction: {} !".format(direction))
+    return np.array(coordinates)
+
+
+def finish_exhibit(initial_black_tiles, n_days, supported_directions):
+    floor_plan = get_floor_plan(initial_black_tiles, n_days)
+    neighbor_mask = get_neighbor_mask(supported_directions)
+    for i in range(n_days):
+        floor_plan = next(floor_plan, neighbor_mask)
+    return floor_plan
+
+
+def get_floor_plan(initial_black_tiles, n_days):
+    initial_dimensions = np.max(np.array(initial_black_tiles), axis=0) - np.min(np.array(initial_black_tiles), axis=0)
+    final_dimensions = initial_dimensions + 2 * (n_days + 1)
+    floor_plan = np.full(final_dimensions, False)
+    for initial_black_tile in initial_black_tiles:
+        floor_plan[initial_black_tile[0] + n_days + 1, initial_black_tile[1] + n_days + 1] = True
+    return floor_plan
+
+
+def get_neighbor_mask(supported_directions):
+    neighbor_mask = np.full((3, 3), False)
+    for direction in supported_directions:
+        coordinates = direction_to_coordinates(direction)
+        neighbor_mask[coordinates[0], coordinates[1]] = True
+    return neighbor_mask
+
+
+def next(floor_plan, neighbor_mask):
+    n_neighbors = convolution2D(floor_plan, neighbor_mask)
+    next_floor_plan = np.logical_or(
+        np.logical_and(floor_plan, np.logical_not(np.logical_or(n_neighbors == 0, n_neighbors > 2))),
+        np.logical_and(np.logical_not(floor_plan), n_neighbors == 2))
+    return next_floor_plan
