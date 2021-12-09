7e91f32fb6158adbe6eb6d9569dd24682a356b38
ColasGael <gael.colas@plus.ai>
2021-12-08 23:30:21 -0800

    [2021] Add solutions for days 8-9

---
+++ 2021/solver/day8.py
@@ -0,0 +1,94 @@
+def main(input_lines):
+    entries = parse_entries(input_lines)
+    part1_answer, part2_answer = solve(entries)
+    return part1_answer, part2_answer
+
+
+def parse_entries(input_lines):
+    entries = []
+    for input_line in input_lines:
+        inputs, outputs = input_line.split("|")
+        signal_patterns = [set(pattern) for pattern in inputs.strip().split(" ")]
+        output_digits = [set(pattern) for pattern in outputs.strip().split(" ")]
+        entries.append((signal_patterns, output_digits))
+    return entries
+
+
+def solve(entries):
+    part1_answer = 0
+    part2_answer = 0
+    for entry in entries:
+        output_number = decode_entry(entry)
+        for i, output_digit in enumerate(output_number):
+            if output_digit in (1, 4, 7, 8):
+                part1_answer += 1
+            part2_answer += output_digit * 10**i
+    return part1_answer, part2_answer
+
+
+def decode_entry(entry):
+    signal_patterns, output_digits = entry
+    # Find the patterns corresponding to the "easy" digits: with a unique number of segments
+    decoded_patterns = {}
+    undecoded_patterns = []
+    for pattern in signal_patterns:
+        if len(pattern) == 2:
+            decoded_patterns[1] = pattern
+        elif len(pattern) == 4:
+            decoded_patterns[4] = pattern
+        elif len(pattern) == 3:
+            decoded_patterns[7] = pattern
+        elif len(pattern) == 7:
+            decoded_patterns[8] = pattern
+        else:
+            undecoded_patterns.append(pattern)
+
+    # Identify which letter corresponds to which segment
+    # The segments are numbered from top to bottom, left to right, as:
+    #  1
+    # 2 3
+    #  4
+    # 5 6
+    #  7
+    segments = {}
+    # Segment 1: is used to draw a 7 but not a 1
+    segments[1] = list(decoded_patterns[7] - decoded_patterns[1])[0]
+    # Segments 1 ; 4 ; 7: are used to draw to all the five-segment digits (2 ; 5)
+    candidate_segments = decoded_patterns[8].intersection(
+        *[pattern for pattern in undecoded_patterns if len(pattern) == 5])
+    # Segment 4: is the only one of these also used to draw a 4
+    segments[4] = list(candidate_segments.intersection(decoded_patterns[4]))[0]
+    # Segment 7: is the remaining one
+    segments[7] = list(candidate_segments - set((segments[1], segments[4])))[0]
+    # Segment 2: is used to draw a 4 but not a 1 ; and not segment 4
+    segments[2] = list(decoded_patterns[4] - decoded_patterns[1] - set((segments[4])))[0]
+    # Segment 5: is not used to draw a 1 ; and not segments 1 ; 2 ; 4 ; 7
+    segments[5] = list(decoded_patterns[8] - decoded_patterns[1] -
+                       set((segments[1], segments[2], segments[4], segments[7])))[0]
+    # Segment 6: is used to draw all the 6-segment digits (0, 6, 9) ; and not segments 1 ; 2 ; 4 ; 7
+    candidate_segments = decoded_patterns[8].intersection(
+        *[pattern for pattern in undecoded_patterns if len(pattern) == 6])
+    segments[6] = list(
+        candidate_segments - set((segments[1], segments[2], segments[4], segments[7])))[0]
+    # Segment 3: is the remaining one used to draw a 1
+    segments[3] = list(decoded_patterns[1] - set((segments[6])))[0]
+
+    # Decode the remaining patterns
+    decoded_patterns.update({
+        0: set((segments[1], segments[2], segments[3], segments[5], segments[6], segments[7])),
+        2: set((segments[1], segments[3], segments[4], segments[5], segments[7])),
+        3: set((segments[1], segments[3], segments[4], segments[6], segments[7])),
+        5: set((segments[1], segments[2], segments[4], segments[6], segments[7])),
+        6: set((segments[1], segments[2], segments[4], segments[5], segments[6], segments[7])),
+        9: set((segments[1], segments[2], segments[3], segments[4], segments[6], segments[7])),
+    })
+
+    # Decypher the output
+    output_number = []
+    for output_digit in output_digits:
+        for digit, pattern in decoded_patterns.items():
+            if output_digit == pattern:
+                output_number.append(digit)
+                break
+    output_number.reverse()
+    return output_number
---
+++ 2021/solver/day9.py
@@ -0,0 +1,87 @@
+def main(input_lines):
+    heightmap = parse_heightmap(input_lines)
+    part1_answer, part2_answer = find_basins(heightmap)
+    return part1_answer, part2_answer
+
+
+def parse_heightmap(input_lines):
+    heightmap = [[int(c) for c in input_line.strip()] for input_line in input_lines]
+    return heightmap
+
+
+def find_basins(heightmap, max_height=9):
+    # the i-th basin is described by the tuple: (number of points, height of low point)
+    basins = []
+    # indicate to which basin the points are associated
+    basins_clusters = []
+    # re-map the basins indices when merging basins
+    basins_idx_remap = {}
+
+    for row_idx, row in enumerate(heightmap):
+        basins_clusters.append([])
+        for col_idx, height in enumerate(row):
+            # Skip the highest points: they are not associated with any basins
+            if height == max_height:
+                basins_clusters[-1].append(-1)
+                continue
+
+            # Find the adjacent points' basins
+            associated_basins = []
+            for neighbour in get_processed_neighbours(row_idx, col_idx):
+                neighbour_basin = basins_clusters[neighbour[0]][neighbour[1]]
+                if neighbour_basin != -1:
+                    associated_basins.append(basins_idx_remap.get(neighbour_basin, neighbour_basin))
+
+            # Create a new basin
+            if len(associated_basins) == 0:
+                basins.append([0, height])
+                basin_idx = len(basins) - 1
+            # Associate the point with the existing basin
+            elif len(associated_basins) == 1:
+                basin_idx = associated_basins[0]
+            # Merge the two existing basins now connecting through the current point
+            else:  # len(associated_basins) == 2
+                if (associated_basins[0] != associated_basins[1]):
+                    basin_idx = merge_basins(
+                        basins, associated_basins[0], associated_basins[1], basins_idx_remap)
+
+            # Update the associated basin
+            update_basin(basins, basins_clusters, basin_idx, height)
+
+    # Clean-up: filter merged basins ; sort the basins by increasing sizes
+    basins = sorted([basin for basin in basins if basin is not None])
+
+    risk_levels_sum = sum([1 + basin[1] for basin in basins])
+    three_largest_basins_sizes_prod = basins[-1][0] * basins[-2][0] * basins[-3][0]
+
+    return risk_levels_sum, three_largest_basins_sizes_prod
+
+
+def get_processed_neighbours(row_idx, col_idx):
+    neighbours = []
+    if row_idx > 0:
+        neighbours.append((row_idx - 1, col_idx))
+    if col_idx > 0:
+        neighbours.append((row_idx, col_idx - 1))
+    return neighbours
+
+
+def update_basin(basins, basins_clusters, basin_idx, height):
+    basins[basin_idx][0] += 1
+    basins[basin_idx][1] = min(basins[basin_idx][1], height)
+    basins_clusters[-1].append(basin_idx)
+
+
+def merge_basins(basins, first_basin_idx, second_basin_idx, basins_idx_remap):
+    new_basin_idx = min(first_basin_idx, second_basin_idx)
+    old_basin_idx = max(first_basin_idx, second_basin_idx)
+
+    basins[new_basin_idx][0] += basins[old_basin_idx][0]
+    basins[new_basin_idx][1] = min(basins[new_basin_idx][1], basins[old_basin_idx][1])
+    basins[old_basin_idx] = None
+
+    basins_idx_remap[old_basin_idx] = new_basin_idx
+    for basin_idx, actual_basin_idx in basins_idx_remap.items():
+        if actual_basin_idx == old_basin_idx:
+            basins_idx_remap[basin_idx] = new_basin_idx
+    return new_basin_idx
